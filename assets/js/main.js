7711795c48274b88ae75cc8fc9587fb1cfae3685
baronmide <danieladegbite@gmail.com>
2022-09-17 18:24:27 +0300

    Continuation of the portfolio website

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+/* =============== MENU SHOW Y HIDDEN ============= */
+const navMenu = document.getElementById("nav__menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+/*========= MENU SHOW ======*/
+/* validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*====== MENU HIDDEN ======*/
+/* validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/* ============ REMOVE MENU MOBILE ============= */
+const navlink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navlink.forEach((n) => n.addEventListener("click", linkAction));
